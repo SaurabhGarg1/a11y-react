46ccb2c9d750ce9079645f88f45ce9722bc9928f
Saurabh Garg <s.garg1@outlook.com>
2021-02-27 14:09:02 +0200

    Menu component - 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import {Menu} from './Menu';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Accessible components</h1>
       </header>
+      <main>
+        <Menu/>
+      </main>
     </div>
   );
 }
---
+++ src/MenuItem.js
@@ -0,0 +1,24 @@
+import { useEffect, useRef } from "react";
+import './MenuItem.css';
+
+const MenuItem = ({item, isSelected, onMouseEnter, onMouseLeave}) => {
+    const itemRef = useRef(null);
+    useEffect(() => {
+        if(isSelected) {
+            itemRef.current.focus();
+        }
+    }, [isSelected])
+    return (
+        <li 
+            role="menuitem"
+            tabIndex={isSelected ? 0 : -1} 
+            ref= {itemRef}
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+            >
+                {item}
+        </li>
+    );
+}
+    
+export default MenuItem
